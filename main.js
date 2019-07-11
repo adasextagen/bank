ba2e7720c36cba6ef85aa01b5af01300e8c9f1dd
Coni <coniarganaraz@hotmail.com>
2019-07-11 19:06:15 -0300

    agrego estilos e imprime info de la tarjeta

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
+    
 /*
-- EL usuario debe poder visualizar su informacion personal y los datos de sus tarjetas de crédito, mostrando una a la vez y permitiendole cambiar entre ellas
-
-- para cada tarjeta se debe mostrar la fecha de vencimiento, la deduda en cada moneda existente y ofrecer el pago en una moneda elejida segun las cuentas de la persona.
-  - Si la persona tiene cuenta en pesos se le debe ofrecer pagar toda su deuda en esa moneda, convirtiendo el valor el valor de     cualquier deuda en otra moneda a pesos y sumandolo al total a pagar.
+- EL usuario debe poder visualizar su informacion personal y los datos de sus tarjetas de crédito, mostrando una a 
+la vez y permitiendole cambiar entre ellas
+- para cada tarjeta se debe mostrar la fecha de vencimiento, la deduda en cada moneda existente y ofrecer el pago en 
+una moneda elejida segun las cuentas de la persona.
+  - Si la persona tiene cuenta en pesos se le debe ofrecer pagar toda su deuda en esa moneda, convirtiendo el valor
+   el valor de     cualquier deuda en otra moneda a pesos y sumandolo al total a pagar.
   - Si la persona tiene cuenta en dolares hay que hace lo mismo en el punto anterior pero convirtiendo todo a dolares. 
-
-- la persona debe poder elegir una opcion de monto a pagar entre total o pago minimo (tiene pago minimo). en caso de hacer el pago minimo hay que mostrar el saldo restante.
-
-tip: las opcionnes de moneda a pagar y monto de pago podrian mostrarse como selectes independientes en los que le sumamos diferentes options segun las condiciones y las opciones conn las que el usuario pueda contar. 
+- la persona debe poder elegir una opcion de monto a pagar entre total o pago minimo (tiene pago minimo). en caso de
+ hacer el pago minimo hay que mostrar el saldo restante.
+tip: las opcionnes de moneda a pagar y monto de pago podrian mostrarse como selectes independientes en los que le
+ sumamos diferentes options segun las condiciones y las opciones conn las que el usuario pueda contar. 
 */
+// datos
 
 let data = {
   user:'Calixta Ochoa',
@@ -42,17 +46,18 @@ let data = {
 
 let currentCard = data.cards[0].brand
 
+// inicializacion del programa
 const initialize = () => {
   printInitialData()
   printCurrentCard()
 }
 
 const printInitialData = () => {
-  console.log(currentCard)
-  let h1 = document.getElementById('greet')
+ // console.log(currentCard)
+  let userGreet = document.getElementById('greet')
   let select = document.getElementById('selectCard')
   select.innerHTML = ''
-  h1.innerText = `Hola ${data.user}`
+  userGreet.innerText = `Bienvenido/a ${data.user}`
 
   data.cards.forEach( card => {
     let option = document.createElement('option')
@@ -69,12 +74,28 @@ const printCurrentCard = () => {
   let card = data.cards.find( 
     e => e.brand === currentCard
   )
+  // creacion de elementos con informacion de la tarjeta
   let title = document.createElement('h3')
-  let date = document.createElement('em')
+  let date = document.createElement('p')
+  let pesosBalance = document.createElement('p')
+  let dollarsBalance = document.createElement('p')
   title.innerText = card.brand
-  date.innerText = card.expirationDate
+  date.innerText = `Fecha de expiración: ${card.expirationDate}`
+  pesosBalance.innerText = `Saldo a pagar en pesos: $ ${card.pesosDebt}`
+  dollarsBalance.innerText = `Saldo a pagar en dólares: US$ ${card.dollarsDebt}`
   container.appendChild(title)
   container.appendChild(date)
+  // hasta acá apendea si o si (nombre tarjeta y expiracion)
+
+  // funcion para saber si tiene o no cuenta en pesos/dolares - ver como mejorar
+  if (card.pesosDebt >0){
+    container.appendChild(pesosBalance)
+  } else {null}
+
+  if (card.dollarsDebt >0){
+    container.appendChild(dollarsBalance)
+  } else {null}
+
 }
 
 const changeCard = () => {
